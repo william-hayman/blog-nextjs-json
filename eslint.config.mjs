f484dcaf24cdcf7d748c6c721a0ff03473efe416
william-hayman <126586623+william-hayman@users.noreply.github.com>
2024-12-27 16:30:17 +0400

    Add Blog

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,14 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals"),
+  {
+    files: ["**/*.js", "**/*.jsx"], // Apenas arquivos JS/JSX
+    languageOptions: {
+      parser: "espree", // Parser padrão do JavaScript
+    },
+  },
+];
 
 export default eslintConfig;
