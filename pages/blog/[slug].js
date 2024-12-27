67ff80b66eca9766e011476b6d33fca67e39ed08
william-hayman <126586623+william-hayman@users.noreply.github.com>
2024-12-27 17:45:14 +0400

    add Flowbite Navbar

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,6 +1,7 @@
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 import { BLOCKS } from '@contentful/rich-text-types';
 import client from '@/lib/contentful';
+import {NavBar} from "@/components/navbar";
 
 const getAssetUrl = async (assetId) => {
     const asset = await client.getAsset(assetId);
@@ -68,6 +69,7 @@ export default function PostDetails({ post, thumbnailUrl }) {
 
     return (
         <div>
+<NavBar />
             <h1>{title}</h1>
             {thumbnailUrl && <img src={thumbnailUrl} alt={title} />}
             <p><strong>Autor:</strong> {author[0]}</p>
